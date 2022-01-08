190664c53710ad21fe8dec5c9a826587c891e648
raju2404 <70800347+raju2404@users.noreply.github.com>
2022-01-08 21:15:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ mysql = MySQL()
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'Sachin@200'
-app.config['MYSQL_DATABASE_DB'] = 'app_test_db'
-app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'
+app.config['MYSQL_DATABASE_DB'] = 'db_test_scanify'
+app.config['MYSQL_DATABASE_HOST'] = '35.193.62.223'
 
 
 
@@ -30,7 +30,7 @@ def hello():
 @app.route('/getEntities')
 def getEntities():
     cur = mysql.connect().cursor()
-    cur.execute('''select * from app_test_db.ENTITIES''')
+    cur.execute('''select * from db_test_scanify.ENTITIES''')
     r = [dict((cur.description[i][0], value)
                 for i, value in enumerate(row)) for row in cur.fetchall()]
     return jsonify({'myCollection' : r})
